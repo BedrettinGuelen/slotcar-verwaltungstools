ce111d3949f32e583b30670d1f64d54f8df068ab
Bedrettin Gülen <bedrettingulen@MacBook-Pro.fritz.box>
2024-04-27 23:28:58 +0200

    delete etwas

--- src/Controller/OriginalCarController.php
+++ src/Controller/OriginalCarController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\DTO\SearchRequestDto;
 use App\Entity\OriginalCar;
 use App\Form\OriginalCarType;
 use App\Repository\OriginalCarRepository;
@@ -11,7 +10,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\SerializerInterface;
 
 
 class OriginalCarController extends AbstractController
